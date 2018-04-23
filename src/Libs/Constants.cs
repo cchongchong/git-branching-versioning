c2026786cbf7bc2211ec07199b178eca39a674c3
Chongzhao Wang <cchongchong@users.noreply.github.com>
2018-04-23 13:34:36 -0400

    Fix feature 2 after pulled to release

--- src/Libs/Constants.cs
+++ src/Libs/Constants.cs
@@ -3,6 +3,6 @@
     public class Constants
     {
         public const string Feature1 = "Feature 1 added.";
-        public const string Feature2 = "Feature 2 added.";
+        public const string Feature2 = "Feature 2 added. Bug fixed after pulled to release.";
     }
 }
